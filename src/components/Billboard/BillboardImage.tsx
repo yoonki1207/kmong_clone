752108aa0c65f97a4b0e95b69e8a2b581d1cca3d
yoonki1207 <yoonki1207@github.com>
2023-03-07 19:45:25 +0900

    Design update

--- src/components/Billboard/BillboardImage.tsx
+++ src/components/Billboard/BillboardImage.tsx
@@ -3,74 +3,74 @@ import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 interface Props {
-  images: {
-    color: string;
-    src: string;
-  }[];
-  imageIndex: number;
-  setImageIndex: (index: React.SetStateAction<number>) => void;
+	images: {
+		color: string;
+		src: string;
+	}[];
+	imageIndex: number;
+	setImageIndex: (index: React.SetStateAction<number>) => void;
 }
 const offsetLeftList = [0, 540, 1080, 1620];
 const BillboardImage = ({ images, imageIndex, setImageIndex }: Props) => {
-  const isActiveImgLoop = useRef<boolean>(true);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (isActiveImgLoop.current) {
-        setImageIndex((prev) => prev + 1);
-      }
-    }, 2000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+	const isActiveImgLoop = useRef<boolean>(true);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (isActiveImgLoop.current) {
+				setImageIndex((prev) => prev + 1);
+			}
+		}, 4000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
 
-  const onMouseEnter = () => {
-    isActiveImgLoop.current = false;
-  };
+	const onMouseEnter = () => {
+		isActiveImgLoop.current = false;
+	};
 
-  const onMouseLeave = () => {
-    isActiveImgLoop.current = true;
-  };
+	const onMouseLeave = () => {
+		isActiveImgLoop.current = true;
+	};
 
-  return (
-    <>
-      <div className="img_container">
-        <div
-          className="img_layout"
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-          style={{
-            left: -offsetLeftList[imageIndex % offsetLeftList.length],
-          }}
-        >
-          {images.map((val) => (
-            <img key={val.src} src={val.src} height={380} />
-          ))}
-        </div>
-        <div className="img_index">{`${imageIndex % images.length}/${
-          images.length
-        }`}</div>
-      </div>
-      <button
-        className="arrow_left"
-        onClick={() => {
-          setImageIndex((prev) => prev + images.length - 1);
-        }}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        <KeyboardArrowLeftIcon />
-      </button>
-      <button
-        className="arrow_right"
-        onClick={() => {
-          setImageIndex((prev) => prev + 1);
-        }}
-      >
-        <KeyboardArrowRightIcon />
-      </button>
-    </>
-  );
+	return (
+		<>
+			<div className="img_container">
+				<div
+					className="img_layout"
+					onMouseEnter={onMouseEnter}
+					onMouseLeave={onMouseLeave}
+					style={{
+						left: -offsetLeftList[imageIndex % offsetLeftList.length],
+					}}
+				>
+					{images.map((val) => (
+						<img key={val.src} src={val.src} height={380} />
+					))}
+				</div>
+				<div className="img_index">{`${(imageIndex % images.length) + 1}/${
+					images.length
+				}`}</div>
+			</div>
+			<button
+				className="arrow_left"
+				onClick={() => {
+					setImageIndex((prev) => prev + images.length - 1);
+				}}
+				onMouseEnter={onMouseEnter}
+				onMouseLeave={onMouseLeave}
+			>
+				<KeyboardArrowLeftIcon />
+			</button>
+			<button
+				className="arrow_right"
+				onClick={() => {
+					setImageIndex((prev) => prev + 1);
+				}}
+			>
+				<KeyboardArrowRightIcon />
+			</button>
+		</>
+	);
 };
 
 export default BillboardImage;
--- src/components/Billboard/TypingAnimation.tsx
+++ src/components/Billboard/TypingAnimation.tsx
@@ -4,69 +4,69 @@ import { EAnimate } from "./Billboard";
 
 const messages: string[] = ["쇼핑몰", "전자책", "홈페이지", "VOD", "심리상담"];
 interface Props {
-  animateState: any;
-  setAnimateState: any;
+	animateState: any;
+	setAnimateState: any;
 }
 const TypingAnimation = ({ animateState, setAnimateState }: Props) => {
-  const messageIndex = useRef(0);
-  const [searchKeyword, setSearchKeyword] = useState("");
-  useEffect(() => {
-    if (animateState === EAnimate.READY) {
-      setAnimateState(EAnimate.TYPING);
-      const msg = messages[messageIndex.current % messages.length]
-        .split("")
-        .map((val) => toKorChars(val));
-      var isStart = false;
-      const msgRecursive = (msg: string[][]) => {
-        if (msg && msg.length >= 1) {
-          const c = msg[0][0];
-          const isLongC = msg[0].length > 1;
-          if (isStart) {
-            setSearchKeyword((prev) => prev.substring(0, prev.length - 1) + c);
-            msg[0].splice(0, 1);
-            if (msg[0].length === 0) {
-              isStart = false;
-              msg.splice(0, 1);
-            }
-          } else if (isLongC) {
-            if (!isStart) {
-              setSearchKeyword((prev) => prev + c);
-              msg[0].splice(0, 1);
-              isStart = true;
-            }
-          } else {
-            setSearchKeyword((prev) => prev + c);
-            msg.splice(0, 1);
-          }
-        } else {
-          clearInterval(interval);
-          setAnimateState(EAnimate.DELAY);
-          messageIndex.current++;
-        }
-      };
-      const interval = setInterval(() => msgRecursive(msg), 50);
-    }
-    if (animateState === EAnimate.DELAY) {
-      setTimeout(() => {
-        setAnimateState(EAnimate.ERASING);
-      }, 1000);
-    }
-    if (animateState === EAnimate.ERASING) {
-      var index = searchKeyword.length;
-      const erasing = () => {
-        if (index > 0) {
-          setSearchKeyword((prev) => prev.substring(0, prev.length - 1));
-        } else {
-          clearInterval(erase);
-          setAnimateState(EAnimate.READY);
-        }
-        index--;
-      };
-      const erase = setInterval(erasing, 100);
-    }
-  }, [animateState]);
+	const messageIndex = useRef(0);
+	const [searchKeyword, setSearchKeyword] = useState("");
+	useEffect(() => {
+		if (animateState === EAnimate.READY) {
+			setAnimateState(EAnimate.TYPING);
+			const msg = messages[messageIndex.current % messages.length]
+				.split("")
+				.map((val) => toKorChars(val));
+			var isStart = false;
+			const msgRecursive = (msg: string[][]) => {
+				if (msg && msg.length >= 1) {
+					const c = msg[0][0];
+					const isLongC = msg[0].length > 1;
+					if (isStart) {
+						setSearchKeyword((prev) => prev.substring(0, prev.length - 1) + c);
+						msg[0].splice(0, 1);
+						if (msg[0].length === 0) {
+							isStart = false;
+							msg.splice(0, 1);
+						}
+					} else if (isLongC) {
+						if (!isStart) {
+							setSearchKeyword((prev) => prev + c);
+							msg[0].splice(0, 1);
+							isStart = true;
+						}
+					} else {
+						setSearchKeyword((prev) => prev + c);
+						msg.splice(0, 1);
+					}
+				} else {
+					clearInterval(interval);
+					setAnimateState(EAnimate.DELAY);
+					messageIndex.current++;
+				}
+			};
+			const interval = setInterval(() => msgRecursive(msg), 50);
+		}
+		if (animateState === EAnimate.DELAY) {
+			setTimeout(() => {
+				setAnimateState(EAnimate.ERASING);
+			}, 2500);
+		}
+		if (animateState === EAnimate.ERASING) {
+			var index = searchKeyword.length;
+			const erasing = () => {
+				if (index > 0) {
+					setSearchKeyword((prev) => prev.substring(0, prev.length - 1));
+				} else {
+					clearInterval(erase);
+					setAnimateState(EAnimate.READY);
+				}
+				index--;
+			};
+			const erase = setInterval(erasing, 100);
+		}
+	}, [animateState]);
 
-  return <>{searchKeyword}</>;
+	return <>{searchKeyword}</>;
 };
 
 export default TypingAnimation;
